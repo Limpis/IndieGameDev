6905a726859cd875cafe8e26480b71138a6a3d95
Limpis <linus.andersson91@outlook.com>
2018-09-12 20:21:26 +0200

    Implemented GUI and basic functionality for projects

---
+++ IndieGameDev/Assets/Scripts/Project.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Project : MonoBehaviour {
+
+    [Header("General Information")]
+    [SerializeField]
+    private int smallProjectProblems;
+    [SerializeField]
+    private int mediumProjectProblems, largeProjectProblems;
+
+    private bool activeProject;
+
+    public bool StartNewProject()
+    {
+        if(!activeProject)
+        {
+            activeProject = true;
+            Debug.Log("New project started");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void InitializeSmallProject()
+    {
+
+    }
+
+    public void InitializeMediumProject()
+    {
+
+    }
+
+    public void InitializeLargeProject()
+    {
+
+    }
+
+    public List<int> GetProjectSizeProblems()
+    {
+        List<int> list = new List<int>();
+
+        list.Add(smallProjectProblems);
+        list.Add(mediumProjectProblems);
+        list.Add(largeProjectProblems);
+
+        return list;
+    }
+
+    private void Start()
+    {
+        activeProject = false;
+    }
+}
---
+++ IndieGameDev/Assets/Scripts/ProjectGUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjectGUI : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject projectNewPanel, projectSizePanel, projectProgressPanel;
+    [SerializeField]
+    private List<GameObject> projectSizeProblemsText;
+
+    private Project project;
+
+    public void NewProjectButtonClick()
+    {
+        if(project.StartNewProject())
+        {
+            projectNewPanel.SetActive(false);
+            projectSizePanel.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Error! Trying to start a new project while project already active.");
+        }
+    }
+
+    public void SizeSmallButtonClick()
+    {
+
+    }
+
+    public void SizeMediumButtonClick()
+    {
+
+    }
+
+    public void SizeLargeButtonClick()
+    {
+
+    }
+
+    private void Start()
+    {
+        project = GetComponent<Project>();
+
+        InitializeGUI();
+    }
+
+    private void InitializeGUI()
+    {
+        projectNewPanel.SetActive(true);
+        projectSizePanel.SetActive(false);
+        projectProgressPanel.SetActive(false);
+
+        //Initialize problems per size text labels
+        List<int> list = project.GetProjectSizeProblems();
+        for (int i = 0; i < list.Count; i++)
+        {
+            projectSizeProblemsText[i].GetComponent<Text>().text = "~" + list[i].ToString() + " problems";
+        }
+    }
+}
